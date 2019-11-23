275b25a70cdfc2734a33849807330ffefbdd9427
Andrey Usov <nkt.andr@gmail.com>
2019-11-23 13:36:27 +0300

    logging

--- bot.py
+++ bot.py
@@ -9,51 +9,40 @@ TOKEN_BOT = os.environ.get('TOKEN_BOT')
 TOKEN_API = os.environ.get('TOKEN_API')
 CHAT_ID = os.environ.get('CHAT_ID')
 
+data_headers = {
+    'Authorization': 'Token ' + TOKEN_API
+}
+
+BASE_URL = 'https://dvmn.org'
+BASE_URL_API = BASE_URL + '/api/'
+
+long_polling = BASE_URL_API + 'long_polling/'
+
+bot = telegram.Bot(token=TOKEN_BOT)
+
 
 def main():
+    request_params = {}
     logger = logging.getLogger('NOTICE')
     logger.setLevel(logging.DEBUG)
     logging.basicConfig(filename='app.log', format='[%(asctime)s] : %(name)s : %(levelname)s : [%(message)s]')
-
-    bot = telegram.Bot(token=TOKEN_BOT)
-
     logger.addHandler(LoggerHandler(bot=bot))
     logger.info('Бот успешно запущен')
-    data_headers = {
-        'Authorization': 'Token ' + TOKEN_API
-    }
-
-    request_params = {}
-
-    base_url = 'https://dvmn.org'
-    base_url_api = base_url + '/api/'
-    long_polling = base_url_api + 'long_polling/'
 
     while True:
         try:
             response = requests.get(long_polling, headers=data_headers, params=request_params)
-
             response.raise_for_status()
             dict_response = response.json()
 
             status = dict_response['status']
-            if status == 'found':
-                timestamp = dict_response['last_attempt_timestamp']
-            elif status == 'timeout':
-                timestamp = dict_response['timestamp_to_request']
+            timestamp = get_timestamp(status=status, dict_response=dict_response)
 
             request_params = {'timestamp': timestamp}
 
             answer = dict_response['new_attempts'][0]
-            is_verified = not answer['is_negative']
-            lesson_title = answer['lesson_title']
-            lesson_url = base_url + answer['lesson_url']
 
-            msg = f'У Вас проверили работу "{lesson_title}" {lesson_url} '
-            if is_verified:
-                msg += 'Преподавателю всё понравилось, можно приступать к следущему уроку'
-            else:
-                msg += f'К сожалению в работе нашлись ошибки'
+            msg = get_msg(dict_response=answer)
 
             bot.send_message(chat_id=CHAT_ID, text=msg)
             time.sleep(1)
@@ -64,6 +53,29 @@ def main():
             logger.warning('нет соединения с интернетом')
         except KeyError:
             continue
+        except Exception:
+            logger.exception(msg='Бот упал с ошибкой:', exc_info=True)
+            time.sleep(10)
+
+
+def get_timestamp(status, dict_response):
+    if status == 'found':
+        timestamp = dict_response['last_attempt_timestamp']
+    elif status == 'timeout':
+        timestamp = dict_response['timestamp_to_request']
+    return timestamp
+
+
+def get_msg(dict_response):
+    is_verified = not dict_response['is_negative']
+    lesson_title = dict_response['lesson_title']
+    lesson_url = BASE_URL + dict_response['lesson_url']
+    msg = f'У Вас проверили работу "{lesson_title}" {lesson_url} '
+    if is_verified:
+        msg += 'Преподавателю всё понравилось, можно приступать к следущему уроку'
+    else:
+        msg += f'К сожалению в работе нашлись ошибки'
+    return msg
 
 
 class LoggerHandler(logging.Handler):
